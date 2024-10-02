b3aef649bfa02282273f19cef3897be8cf869b01
Joseph DeMarco <117295065+Josephmd2k4@users.noreply.github.com>
2024-10-02 03:30:13 +0000

    k

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,15 @@
 import streamlit as st
-
+from PIL import Image
+import torch
+from torchvision import transforms
+import numpy as np
 st.title("Thyroid Cancer")
-st.write(
-    "Upload photo below:"
-)
 menu = ["Image","Dataset","About"]
 choice = st.sidebar.selectbox("Menu",menu)
-st.file_uploader("Image must be 24x24 in PNG format")
+uploaded_file = st.file_uploader("Upload Photo Below:")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption = 'Uploaded Image.', use_column_width=True)
+    st.write("")
+    st.write("Predicting...")
+    processed_image = preprocess_image(image)

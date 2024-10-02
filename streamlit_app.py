8b58540fdebefb5bdbf705402585b8c8d7f41e1f
Joseph DeMarco <117295065+Josephmd2k4@users.noreply.github.com>
2024-10-02 16:08:12 +0000

    take out the line that says predicting

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,6 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
-    st.write("Predicting...")
     processed_image = preprocess_image(image)
 
     # Run inference with PyTorch model
